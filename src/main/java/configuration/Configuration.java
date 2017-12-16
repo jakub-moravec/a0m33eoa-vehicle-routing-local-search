61cae62ca78aaa4f16b48228e24fd4f46962649b
Jakub Moravec <jakub.moravec@profinit.eu>
2017-12-16 19:50:57 +0100

    changed config

--- src/main/java/configuration/Configuration.java
+++ src/main/java/configuration/Configuration.java
@@ -43,7 +43,7 @@ public class Configuration {
     // parameters
     @Getter
     @Setter
-    private static String outputFile = "output.txt";
+    private static String outputFile = "C:\\Users\\jmoravec\\Desktop\\results\\output.txt";
 
     @Getter
     @Setter
